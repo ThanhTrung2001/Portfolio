a0b8f6de7bb80cb5818c471848440b12b4d448e6
Izu <thanhtrungtran13@gmail.com>
2023-11-21 14:25:59 +0700

    refactor: setup firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
+import { LayoutModule } from './layout/layout.module';
+
+import { provideFirebaseApp, getApp, initializeApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { environment } from './core/environtment/environtment';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CoreModule,
+    SharedModule,
+    LayoutModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore()),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/core/environtment/environtment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyArmdOsLjWz3H4tTmQLcZSnN6VNieGzdYI',
+    authDomain: 'chew-socialnetwork.firebaseapp.com',
+    projectId: 'chew-socialnetwork',
+    storageBucket: 'chew-socialnetwork.appspot.com',
+    messagingSenderId: '541858929870',
+    appId: '1:541858929870:web:1e8c22ff63765498cdc49f',
+    measurementId: 'G-7C35L9M8HS',
+  },
+};
